a7ead3f4bdbaaab27b96355b9ddc79c214d16d73
heun <heun210@gmail.com>
2022-05-04 16:00:03 +0900

    Enter

---
+++ homework/memo/main.js
@@ -0,0 +1,11 @@
+const postBox = document.querySelector(".btn");
+
+/* function onClick() {
+  alert("안녕!");
+}
+
+postBox.addEventListener("click", onClick); */
+
+postBox.addEventListener("click", () => {
+  alert("안녕!");
+});
