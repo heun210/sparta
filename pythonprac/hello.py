73426f4f67c722c9cad88095d3db683f71b5da8b
heun <heun210@gmail.com>
2022-05-10 15:51:00 +0900

    Enter

---
+++ pythonprac/hello.py
@@ -0,0 +1,13 @@
+people = [{'name': 'bob', 'age': 15}, 
+          {'name': 'carry', 'age': 18},
+          {'name': 'john', 'age': 11}]
+
+
+for person in people :
+    #print(person)
+    
+    #print(person['name'])
+    
+    if person['age'] <16 :
+        print(person)
+    
