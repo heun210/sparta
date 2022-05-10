bcc0edf33e106e36fc5ccaf10f51022f8e8c46d2
heun <heun210@gmail.com>
2022-05-10 17:51:47 +0900

    Enter

--- pythonprac/hello.py
+++ pythonprac/hello.py
@@ -1,13 +1,19 @@
-people = [{'name': 'bob', 'age': 15}, 
-          {'name': 'carry', 'age': 18},
-          {'name': 'john', 'age': 11}]
+import requests
+from bs4 import BeautifulSoup
+
+headers = {'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
+data = requests.get('https://movie.naver.com/movie/sdb/rank/rmovie.nhn?sel=pnt&date=20200303',headers=headers)
+
+soup = BeautifulSoup(data.text, 'html.parser')
 
 
-for person in people :
-    #print(person)
-    
-    #print(person['name'])
-    
-    if person['age'] <16 :
-        print(person)
-    
+movies = soup.select('#old_content > table > tbody > tr')
+
+for movie in movies:
+    # movie 안에 a 가 있으면,
+     a_tag = movie.select_one('td.title > div > a')
+     if a_tag is not None:
+         rank=movie.select_one('td:nth-child(1) > img')['alt']
+         title=a_tag.text
+         point=movie.select_one('td.point').text
+         print(rank,title,point)
