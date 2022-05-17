c344bda3eff056a8986a98a6ed533d7e15d54889
heun <heun210@gmail.com>
2022-05-17 17:58:10 +0900

    Enter

---
+++ homework/1week-물개/templates/main.js
@@ -0,0 +1,23 @@
+const btn = document.querySelector(".btn");
+const price = document.querySelector("#price");
+$(document).ready(() => {
+  getRate();
+});
+
+function getRate() {
+  $.ajax({
+    type: "GET",
+    url: "http://spartacodingclub.shop/sparta_api/rate",
+    data: {},
+    success: function (response) {
+      let nowRate = response["rate"];
+      $("#price").text(nowRate);
+    },
+  });
+}
+
+function onClick() {
+  alert("주문이 완료되었습니다");
+}
+
+btn.addEventListener("click", onClick);
